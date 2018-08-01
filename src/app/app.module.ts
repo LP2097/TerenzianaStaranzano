025d2ad4eefa1c11651e9e98ce550c3f60dfa0fd
Luca Peruzzo <peruzzo.luca1@gmail.com>
2018-08-01 09:31:56 +0200

    Aggiunta componenti base progetto e Bootstrap versione 3.3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SquadComponent } from './squad/squad.component';
+import { HistoryComponent } from './history/history.component';
+import { StaffComponent } from './staff/staff.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    SquadComponent,
+    HistoryComponent,
+    StaffComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
